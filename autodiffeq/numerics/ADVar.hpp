1daadd723ef3e6589e50c719431739ca7c402438
Savithru Jayasinghe <43912545+savithru-j@users.noreply.github.com>
2023-01-22 22:34:57 -0500

    Implemented several operators for ADVar and added unit tests

--- autodiffeq/numerics/ADVar.hpp
+++ autodiffeq/numerics/ADVar.hpp
@@ -11,6 +11,8 @@ namespace autodiffeq
 {
 
 // ADVar : Auto-diff variable for storing a value and a vector of derivatives
+template<typename T>
+class ADVar;
 
 template<typename T>
 class ADVar
@@ -18,12 +20,12 @@ class ADVar
 public:
   ADVar() = default;
 
-  ADVar(const T& v, const T d[], const int n)
+  ADVar(const T& v, const T d[], const int num_deriv)
   {
-    assert(n > 0);
+    assert(num_deriv > 0);
     v_ = v;
-    d_ = new T[n];
-    N_ = n;
+    d_ = new T[num_deriv];
+    N_ = num_deriv;
     for (unsigned int i = 0; i < N_; ++i)
       d_[i] = d[i];
   }
@@ -44,10 +46,21 @@ public:
   explicit ADVar(const T& v, const int num_deriv) : v_(v) 
   {
     N_ = num_deriv;
-    d_ = new T[N_];
+    if (N_ > 0)
+      d_ = new T[N_];
   }
   
   ADVar(const T& v) : v_(v), d_(nullptr), N_(0) {}
+
+  ADVar(const ADVar& var) : v_(var.v_), N_(var.N_)
+  {
+    if (N_ > 0) 
+    {
+      d_ = new T[N_];
+      for (unsigned int i = 0; i < N_; ++i)
+        d_[i] = var.d_[i];
+    }
+  }
 
   ~ADVar() { delete [] d_; }
 
@@ -65,6 +78,38 @@ public:
   }
   inline T deriv(int i = 0) const { return N_ > 0 ? d_[i] : T(0.0); }
 
+  // assignment operators
+  ADVar& operator=(const ADVar& var);
+  ADVar& operator=(const T& v);
+
+  // unary operators
+  const ADVar& operator+() const;
+  const ADVar  operator-() const;
+
+  // binary accumulation operators
+  ADVar& operator+=(const ADVar& var);
+  ADVar& operator+=(const T& v);
+  ADVar& operator-=(const ADVar& var);
+  ADVar& operator-=(const T& v);
+  ADVar& operator*=(const ADVar& var);
+  ADVar& operator*=(const T& v);
+  ADVar& operator/=(const ADVar& var);
+  ADVar& operator/=(const T& v);
+
+  // binary operators
+  template<typename U> friend ADVar<U> operator+( const ADVar<U>& a, const ADVar<U>& b);
+  template<typename U> friend ADVar<U> operator+( const ADVar<U>& a, const U& b);
+  template<typename U> friend ADVar<U> operator+( const U& a, const ADVar<U>& b);
+  template<typename U> friend ADVar<U> operator-( const ADVar<U>& a, const ADVar<U>& b);
+  template<typename U> friend ADVar<U> operator-( const ADVar<U>& a, const U& b);
+  template<typename U> friend ADVar<U> operator-( const U& a, const ADVar<U>& b);
+  template<typename U> friend ADVar<U> operator*( const ADVar<U>& a, const ADVar<U>& b);
+  template<typename U> friend ADVar<U> operator*( const ADVar<U>& a, const U& b);
+  template<typename U> friend ADVar<U> operator*( const U& a, const ADVar<U>& b);
+  template<typename U> friend ADVar<U> operator/( const ADVar<U>& a, const ADVar<U>& b);
+  template<typename U> friend ADVar<U> operator/( const ADVar<U>& a, const U& b);
+  template<typename U> friend ADVar<U> operator/( const U& a, const ADVar<U>& b);
+
 protected:
 
   T v_; //value
@@ -72,5 +117,375 @@ protected:
   unsigned int N_; //number of derivatives
 };
 
+// assignment
+template<typename T>
+ADVar<T>& ADVar<T>::operator=(const ADVar& var)
+{
+  //Do nothing if assigning self to self
+  if ( &var == this ) return *this;
+
+  if ((N_ == 0) && (var.N_ > 0))
+  {
+    N_ = var.N_;
+    d_ = new T[N_];
+  }
+  else if ( var.N_ == 0 )
+  {
+    (*this) = var.v_; //var is a scalar
+    return *this;
+  }
+  else
+    assert(N_ == var.N_);
+
+  v_ = var.v_;
+  for (unsigned int i = 0; i < N_; i++)
+    d_[i] = var.d_[i];
+  return *this;
+}
+
+template<typename T>
+ADVar<T>& ADVar<T>::operator=(const T& v)
+{
+  v_ = v;
+  delete [] d_;
+  d_ = nullptr;
+  N_ = 0;
+  return *this;
+}
+
+// unary operators
+template<typename T>
+const ADVar<T>& ADVar<T>::operator+() const
+{
+  return *this;
+}
+
+template<typename T>
+const ADVar<T> ADVar<T>::operator-() const
+{
+  ADVar c(-v_, N_);
+  for (unsigned int i = 0; i < N_; i++)
+    c.d_[i] = -d_[i];
+  return c;
+}
+
+// binary accumulation operators
+template<typename T>
+ADVar<T>& ADVar<T>::operator+=(const ADVar& var)
+{
+  if ((N_ == 0) && (var.N_ > 0))
+  {
+    N_ = var.N_;
+    d_ = new T[N_](); //d_ is value-initialized
+  }
+  else if (var.N_ == 0)
+  {
+    v_ += var.v_; //var is a scalar
+    return *this;
+  }
+  else
+    assert(N_ == var.N_);
+
+  v_ += var.v_;
+  for (unsigned int i = 0; i < N_; i++)
+    d_[i] += var.d_[i];
+  return *this;
+}
+
+template<typename T>
+ADVar<T>& ADVar<T>::operator+=(const T& v)
+{
+  v_ += v;
+  return *this;
+}
+
+template<typename T>
+ADVar<T>& ADVar<T>::operator-=(const ADVar& var)
+{
+  if ((N_ == 0) && (var.N_ > 0))
+  {
+    N_ = var.N_;
+    d_ = new T[N_](); //d_ is value-initialized
+  }
+  else if (var.N_ == 0)
+  {
+    v_ -= var.v_; //var is a scalar
+    return *this;
+  }
+  else
+    assert(N_ == var.N_);
+
+  v_ -= var.v_;
+  for (unsigned int i = 0; i < N_; i++)
+    d_[i] -= var.d_[i];
+  return *this;
+}
+
+template<typename T>
+ADVar<T>& ADVar<T>::operator-=(const T& v)
+{
+  v_ -= v;
+  return *this;
+}
+
+template<typename T>
+ADVar<T>& ADVar<T>::operator*=(const ADVar& var)
+{
+  if ((N_ == 0) && (var.N_ > 0))
+  {
+    N_ = var.N_;
+    d_ = new T[N_](); //d_ is value-initialized
+  }
+  else if (var.N_ == 0)
+  {
+    v_ *= var.v_; //var is a scalar
+    return *this;
+  }
+  else
+    assert(N_ == var.N_);
+
+  for (unsigned int i = 0; i < N_; i++)
+    d_[i] = v_*var.d_[i] + d_[i]*var.v_;
+  v_ *= var.v_;
+  return *this;
+}
+
+template<typename T>
+ADVar<T>& ADVar<T>::operator*=(const T& v)
+{
+  for (unsigned int i = 0; i < N_; i++)
+    d_[i] *= v;
+  v_ *= v;
+  return *this;
+}
+
+template<typename T>
+ADVar<T>& ADVar<T>::operator/=(const ADVar& var)
+{
+  if ((N_ == 0) && (var.N_ > 0))
+  {
+    N_ = var.N_;
+    d_ = new T[N_](); //d_ is value-initialized
+  }
+  else if (var.N_ == 0)
+  {
+    v_ /= var.v_; //var is a scalar
+    return *this;
+  }
+  else
+    assert(N_ == var.N_);
+
+  T tmp = 1.0/(var.v_*var.v_);
+  for (unsigned int i = 0; i < N_; i++)
+    d_[i] = (d_[i]*var.v_ - v_*var.d_[i]) * tmp;
+  v_ /= var.v_;
+  return *this;
+}
+
+template<typename T>
+ADVar<T>& ADVar<T>::operator/=(const T& v)
+{
+  T tmp = 1.0/v;
+  for (unsigned int i = 0; i < N_; i++)
+    d_[i] *= tmp;
+  v_ *= tmp;
+  return *this;
+}
+
+//binary operators
+template<typename T>
+ADVar<T> operator+(const ADVar<T>& a, const ADVar<T>& b)
+{
+  if (a.N_ == 0 && b.N_ == 0)
+    return ADVar<T>(a.v_ + b.v_);
+  else if (a.N_ > 0 && b.N_ == 0)
+    return ADVar<T>(a.v_ + b.v_, a.d_, a.N_);
+  else if (a.N_ == 0 && b.N_ > 0)
+    return ADVar<T>(a.v_ + b.v_, b.d_, b.N_);
+  
+  assert(a.N_ == b.N_);
+  ADVar<T> c(a.v_ + b.v_, a.N_);
+  for (unsigned int i = 0; i < c.N_; i++)
+    c.d_[i] = a.d_[i] + b.d_[i];
+  return c;
+}
+
+template<typename T>
+ADVar<T> operator+(const ADVar<T>& a, const T& b)
+{
+  if (a.N_ == 0 )
+    return ADVar<T>(a.v_ + b);
+  else
+    return ADVar<T>(a.v_ + b, a.d_, a.N_);
+}
+
+template<typename T>
+ADVar<T> operator+(const T& a, const ADVar<T>& b)
+{
+  if (b.N_ == 0 )
+    return ADVar<T>(a + b.v_);
+  else
+    return ADVar<T>(a + b.v_, b.d_, b.N_);
+}
+
+template<typename T>
+ADVar<T> operator-(const ADVar<T>& a, const ADVar<T>& b)
+{
+  if (a.N_ == 0 && b.N_ == 0)
+    return ADVar<T>(a.v_ - b.v_);
+  else if (a.N_ > 0 && b.N_ == 0)
+    return ADVar<T>(a.v_ - b.v_, a.d_, a.N_);
+  else if (a.N_ == 0 && b.N_ > 0)
+  {
+    ADVar<T> c(a.v_ - b.v_, b.N_);
+    for (unsigned int i = 0; i < b.N_; i++)
+      c.d_[i] = -b.d_[i];
+    return c;
+  }
+  
+  assert(a.N_ == b.N_);
+  ADVar<T> c(a.v_ - b.v_, a.N_);
+  for (unsigned int i = 0; i < c.N_; i++)
+    c.d_[i] = a.d_[i] - b.d_[i];
+  return c;
+}
+
+template<typename T>
+ADVar<T> operator-(const ADVar<T>& a, const T& b)
+{
+  if (a.N_ == 0 )
+    return ADVar<T>(a.v_ - b);
+  else
+    return ADVar<T>(a.v_ - b, a.d_, a.N_);
+}
+
+template<typename T>
+ADVar<T> operator-(const T& a, const ADVar<T>& b)
+{
+  if (b.N_ == 0 )
+    return ADVar<T>(a - b.v_);
+  else
+  {
+    ADVar<T> c(a - b.v_, b.N_);
+    for (unsigned int i = 0; i < b.N_; i++)
+      c.d_[i] = -b.d_[i];
+    return c;
+  }
+}
+
+template<typename T>
+ADVar<T> operator*(const ADVar<T>& a, const ADVar<T>& b)
+{
+  if (a.N_ == 0 && b.N_ == 0)
+    return ADVar<T>(a.v_ * b.v_);
+  else if (a.N_ > 0 && b.N_ == 0)
+  {
+    ADVar<T> c(a.v_ * b.v_, a.N_);
+    for (unsigned int i = 0; i < a.N_; i++)
+      c.d_[i] = a.d_[i] * b.v_;
+    return c;
+  }
+  else if (a.N_ == 0 && b.N_ > 0)
+  {
+    ADVar<T> c(a.v_ * b.v_, b.N_);
+    for (unsigned int i = 0; i < b.N_; i++)
+      c.d_[i] = a.v_ * b.d_[i];
+    return c;
+  }
+  
+  assert(a.N_ == b.N_);
+  ADVar<T> c(a.v_ * b.v_, a.N_);
+  for (unsigned int i = 0; i < c.N_; i++)
+    c.d_[i] = a.d_[i]*b.v_ + a.v_*b.d_[i];
+  return c;
+}
+
+template<typename T>
+ADVar<T> operator*(const ADVar<T>& a, const T& b)
+{
+  if (a.N_ == 0 )
+    return ADVar<T>(a.v_ * b);
+  else
+  {
+    ADVar<T> c(a.v_ * b, a.N_);
+    for (unsigned int i = 0; i < a.N_; i++)
+      c.d_[i] = a.d_[i] * b;
+    return c;
+  }
+}
+
+template<typename T>
+ADVar<T> operator*(const T& a, const ADVar<T>& b)
+{
+  if (b.N_ == 0 )
+    return ADVar<T>(a * b.v_);
+  else
+  {
+    ADVar<T> c(a * b.v_, b.N_);
+    for (unsigned int i = 0; i < b.N_; i++)
+      c.d_[i] = a * b.d_[i];
+    return c;
+  }
+}
+
+template<typename T>
+ADVar<T> operator/(const ADVar<T>& a, const ADVar<T>& b)
+{
+  if (a.N_ == 0 && b.N_ == 0)
+    return ADVar<T>(a.v_ / b.v_);
+  else if (a.N_ > 0 && b.N_ == 0)
+  {
+    ADVar<T> c(a.v_ / b.v_, a.N_);
+    T tmp = 1.0 / b.v_;
+    for (unsigned int i = 0; i < a.N_; i++)
+      c.d_[i] = a.d_[i] * tmp;
+    return c;
+  }
+  else if (a.N_ == 0 && b.N_ > 0)
+  {
+    ADVar<T> c(a.v_ / b.v_, b.N_);
+    T tmp = 1.0 / (b.v_ * b.v_);
+    for (unsigned int i = 0; i < b.N_; i++)
+      c.d_[i] = -a.v_ * b.d_[i] * tmp;
+    return c;
+  }
+  
+  assert(a.N_ == b.N_);
+  ADVar<T> c(a.v_ / b.v_, a.N_);
+  T tmp = 1.0 / (b.v_ * b.v_);
+  for (unsigned int i = 0; i < c.N_; i++)
+    c.d_[i] = (a.d_[i]*b.v_ - a.v_*b.d_[i]) * tmp;
+  return c;
+}
+
+template<typename T>
+ADVar<T> operator/(const ADVar<T>& a, const T& b)
+{
+  if (a.N_ == 0 )
+    return ADVar<T>(a.v_ / b);
+  else
+  {
+    ADVar<T> c(a.v_ / b, a.N_);
+    T tmp = 1.0 / b;
+    for (unsigned int i = 0; i < a.N_; i++)
+      c.d_[i] = a.d_[i] * tmp;
+    return c;
+  }
+}
+
+template<typename T>
+ADVar<T> operator/(const T& a, const ADVar<T>& b)
+{
+  if (b.N_ == 0 )
+    return ADVar<T>(a / b.v_);
+  else
+  {
+    ADVar<T> c(a / b.v_, b.N_);
+    T tmp = 1.0 / (b.v_ * b.v_);
+    for (unsigned int i = 0; i < b.N_; i++)
+      c.d_[i] = -a * b.d_[i] * tmp;
+    return c;
+  }
+}
 
 }
--- test/numerics/ADVar_test.cpp
+++ test/numerics/ADVar_test.cpp
@@ -1,3 +1,7 @@
+// autodiffeq - C++ library for sensitivity analysis of ODEs
+// Licensed under the MIT License (http://opensource.org/licenses/MIT)
+// Copyright (c) 2023, Savithru Jayasinghe
+
 #include <gtest/gtest.h>
 #include <autodiffeq/numerics/ADVar.hpp>
 
@@ -5,7 +9,6 @@
 
 using namespace autodiffeq;
 
-//############################################################################//
 //----------------------------------------------------------------------------//
 TEST( ADVar, Constructor )
 {
@@ -25,6 +28,208 @@ TEST( ADVar, Constructor )
   {
     ADVar<std::complex<double>> var({{42.0, -5.0}}, 2);
     EXPECT_EQ(var.value(), std::complex<double>(42.0, -5.0));
-    EXPECT_EQ(var.size(), 2);
+    EXPECT_EQ(var.size(), 2u);
+  }
+  {
+    ADVar<int> var(4, {3, -1, 7, 2});
+    auto var2(var);
+    EXPECT_EQ(var2.value(), 4);
+    EXPECT_EQ(var2.size(), 4u);
+    EXPECT_EQ(var2.deriv(0), 3);
+    EXPECT_EQ(var2.deriv(1), -1);
+    EXPECT_EQ(var2.deriv(2), 7);
+    EXPECT_EQ(var2.deriv(3), 2);
+  }
+}
+
+//----------------------------------------------------------------------------//
+TEST( ADVar, Assignment )
+{
+  {
+    ADVar<double> var(0.0, 3);
+    var = ADVar<double>(2, {3, 0, 1});
+    EXPECT_EQ(var.value(), 2.0);
+    EXPECT_EQ(var.size(), 3u);
+    EXPECT_EQ(var.deriv(0), 3.0);
+    EXPECT_EQ(var.deriv(1), 0.0);
+    EXPECT_EQ(var.deriv(2), 1.0);
+  }
+  {
+    ADVar<double> var(2, {3, 0, 1});
+    ADVar<double> var2 = 35;
+    var = var2;
+    EXPECT_EQ(var.value(), 35.0);
+    EXPECT_EQ(var.size(), 0u);
+  }
+  {
+    ADVar<double> var(0.0, 3);
+    var = 42;
+    EXPECT_EQ(var.value(), 42.0);
+    EXPECT_EQ(var.size(), 0u);
+  }
+}
+
+//----------------------------------------------------------------------------//
+TEST( ADVar, Unary )
+{
+  ADVar<double> var0(2, {3, -0.5, 1});
+
+  auto var = +var0;
+  EXPECT_EQ(var.value(), 2.0);
+  EXPECT_EQ(var.size(), 3u);
+  EXPECT_EQ(var.deriv(0), 3.0);
+  EXPECT_EQ(var.deriv(1), -0.5);
+  EXPECT_EQ(var.deriv(2), 1.0);
+
+  var = -var0;
+  EXPECT_EQ(var.value(), -2.0);
+  EXPECT_EQ(var.size(), 3u);
+  EXPECT_EQ(var.deriv(0), -3.0);
+  EXPECT_EQ(var.deriv(1), 0.5);
+  EXPECT_EQ(var.deriv(2), -1.0);
+}
+
+//----------------------------------------------------------------------------//
+TEST( ADVar, BinaryAccumulation )
+{
+  ADVar<double> var0(2, {3, -4});
+  ADVar<double> var1(7, {-2, 5});
+
+  var0 += var1;
+  EXPECT_EQ(var0.value(), 9.0);
+  EXPECT_EQ(var0.size(), 2u);
+  EXPECT_EQ(var0.deriv(0), 1.0);
+  EXPECT_EQ(var0.deriv(1), 1.0);
+
+  var0 -= var1;
+  EXPECT_EQ(var0.value(), 2.0);
+  EXPECT_EQ(var0.size(), 2u);
+  EXPECT_EQ(var0.deriv(0), 3.0);
+  EXPECT_EQ(var0.deriv(1), -4.0);
+
+  var0 *= var1;
+  EXPECT_EQ(var0.value(), 14.0);
+  EXPECT_EQ(var0.size(), 2u);
+  EXPECT_EQ(var0.deriv(0), 17.0);
+  EXPECT_EQ(var0.deriv(1), -18.0);
+
+  var0 /= var1;
+  EXPECT_DOUBLE_EQ(var0.value(), 2.0);
+  EXPECT_EQ(var0.size(), 2u);
+  EXPECT_DOUBLE_EQ(var0.deriv(0), 3.0);
+  EXPECT_DOUBLE_EQ(var0.deriv(1), -4.0);
+
+  var0 += 9.0;
+  EXPECT_DOUBLE_EQ(var0.value(), 11.0);
+  EXPECT_EQ(var0.size(), 2u);
+  EXPECT_DOUBLE_EQ(var0.deriv(0), 3.0);
+  EXPECT_DOUBLE_EQ(var0.deriv(1), -4.0);
+
+  var0 -= 9.0;
+  EXPECT_DOUBLE_EQ(var0.value(), 2.0);
+  EXPECT_EQ(var0.size(), 2u);
+  EXPECT_DOUBLE_EQ(var0.deriv(0), 3.0);
+  EXPECT_DOUBLE_EQ(var0.deriv(1), -4.0);
+
+  var0 *= 2.0;
+  EXPECT_DOUBLE_EQ(var0.value(), 4.0);
+  EXPECT_EQ(var0.size(), 2u);
+  EXPECT_DOUBLE_EQ(var0.deriv(0), 6.0);
+  EXPECT_DOUBLE_EQ(var0.deriv(1), -8.0);
+
+  var0 /= 2;
+  EXPECT_DOUBLE_EQ(var0.value(), 2.0);
+  EXPECT_EQ(var0.size(), 2u);
+  EXPECT_DOUBLE_EQ(var0.deriv(0), 3.0);
+  EXPECT_DOUBLE_EQ(var0.deriv(1), -4.0);
+}
+
+//----------------------------------------------------------------------------//
+TEST( ADVar, Binary )
+{
+  ADVar<double> var0(2, {3, -4});
+  ADVar<double> var1(7, {-2, 5});
+
+  {
+    auto var = var0 + var1;
+    EXPECT_EQ(var.value(), 9.0);
+    EXPECT_EQ(var.size(), 2u);
+    EXPECT_EQ(var.deriv(0), 1.0);
+    EXPECT_EQ(var.deriv(1), 1.0);
+
+    var = var0 + 3.0;
+    EXPECT_EQ(var.value(), 5.0);
+    EXPECT_EQ(var.size(), 2u);
+    EXPECT_EQ(var.deriv(0), 3.0);
+    EXPECT_EQ(var.deriv(1), -4.0);
+
+    var = -1.0 + var0;
+    EXPECT_EQ(var.value(), 1.0);
+    EXPECT_EQ(var.size(), 2u);
+    EXPECT_EQ(var.deriv(0), 3.0);
+    EXPECT_EQ(var.deriv(1), -4.0);
+  }
+  {
+    auto var = var0 - var1;
+    EXPECT_EQ(var.value(), -5.0);
+    EXPECT_EQ(var.size(), 2u);
+    EXPECT_EQ(var.deriv(0), 5.0);
+    EXPECT_EQ(var.deriv(1), -9.0);
+
+    var = var0 - 3.0;
+    EXPECT_EQ(var.value(), -1.0);
+    EXPECT_EQ(var.size(), 2u);
+    EXPECT_EQ(var.deriv(0), 3.0);
+    EXPECT_EQ(var.deriv(1), -4.0);
+
+    var = 10.0 - var0;
+    EXPECT_EQ(var.value(), 8.0);
+    EXPECT_EQ(var.size(), 2u);
+    EXPECT_EQ(var.deriv(0), -3.0);
+    EXPECT_EQ(var.deriv(1), 4.0);
+
+    var = 1.0 - var0 + var1;
+    EXPECT_EQ(var.value(), 6.0);
+    EXPECT_EQ(var.size(), 2u);
+    EXPECT_EQ(var.deriv(0), -5.0);
+    EXPECT_EQ(var.deriv(1), 9.0);
+  }
+  {
+    auto var = var0 * var1;
+    EXPECT_EQ(var.value(), 14.0);
+    EXPECT_EQ(var.size(), 2u);
+    EXPECT_EQ(var.deriv(0), 17.0);
+    EXPECT_EQ(var.deriv(1), -18.0);
+
+    var = var0 * 3.0;
+    EXPECT_EQ(var.value(), 6.0);
+    EXPECT_EQ(var.size(), 2u);
+    EXPECT_EQ(var.deriv(0), 9.0);
+    EXPECT_EQ(var.deriv(1), -12.0);
+
+    var = -3.0 * var0;
+    EXPECT_EQ(var.value(), -6.0);
+    EXPECT_EQ(var.size(), 2u);
+    EXPECT_EQ(var.deriv(0), -9.0);
+    EXPECT_EQ(var.deriv(1), 12.0);
+  }
+  {
+    auto var = var0 / var1;
+    EXPECT_DOUBLE_EQ(var.value(), 2.0/7.0);
+    EXPECT_EQ(var.size(), 2u);
+    EXPECT_DOUBLE_EQ(var.deriv(0), 25.0/49.0);
+    EXPECT_DOUBLE_EQ(var.deriv(1), -38.0/49.0);
+
+    var = var0 / 5.0;
+    EXPECT_DOUBLE_EQ(var.value(), 2.0/5.0);
+    EXPECT_EQ(var.size(), 2u);
+    EXPECT_DOUBLE_EQ(var.deriv(0), 3.0/5.0);
+    EXPECT_DOUBLE_EQ(var.deriv(1), -4.0/5.0);
+
+    var = 5.0 / var0;
+    EXPECT_DOUBLE_EQ(var.value(), 2.5);
+    EXPECT_EQ(var.size(), 2u);
+    EXPECT_DOUBLE_EQ(var.deriv(0), -3.75);
+    EXPECT_DOUBLE_EQ(var.deriv(1), 5.0);
   }
 }
